15429029dbd32d4ce3aa066a0f94425e664d845e
John <johnsp6@gmail.com>
2024-07-07 19:41:36 +0800

    Added Priority function

--- Program.cs
+++ Program.cs
@@ -3,12 +3,13 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Security.Cryptography;
 
-Dictionary<string, string> ToDoDict = [];
-Dictionary<string, string> ToDoComp = [];
+List<TaskWithPriority> ToDoList = [];
+List<TaskWithPriority> ToDoComp = [];
 
 mainMenu();
 
 void mainMenu(){
+    
     Console.Clear();
     Console.Write("\n    To-Do List\n\n    1. Add Task \n    2. Remove Task  \n    3. View To-Do List \n    4. Completed Tasks\n    5. Exit\n\n    ");
     try {
@@ -27,13 +28,13 @@ void mainMenu(){
             Console.ReadKey();
         } else {
             Console.Clear();
-            Console.WriteLine("\n    To-Do List\n\n    Incorrect option selected. Please Try Again. ");
+            Console.WriteLine("\n    To-Do List\n\n    Incorrect option selected. Please Try Again.\n\n    ");
             Console.ReadKey();
             mainMenu();
         }
     } catch (Exception){
         Console.Clear();
-        Console.WriteLine("\n    To-Do List\n\n    Incorrect input has been inputted. Please Try Again. ");
+        Console.WriteLine("\n    To-Do List\n\n    Incorrect input has been inputted. Please Try Again.\n\n    ");
         Console.ReadKey();
         mainMenu();
     }
@@ -41,6 +42,7 @@ void mainMenu(){
 }
 
 void addTask(){
+
     Console.Clear();
     Console.Write("\n    To-Do List\n\n    What task would you like to add? \n\n    ");
     string? task = Console.ReadLine();
@@ -51,21 +53,30 @@ void addTask(){
         addTask();
     } else {
         Console.Clear();
+        Console.Write("\n    To-Do List\n\n    What is the priority level of this task? \n\n    1. High \n    2. Medium  \n    3. Low \n\n    ");
+        int taskLevel = Convert.ToInt32(Console.ReadLine());
+        if (taskLevel > 3) {
+            Console.Clear();
+            Console.Write("\n    To-Do List\n\n    Incorrect option selected. Please Try Again.\n\n    ");
+            Console.ReadKey();
+            addTask();
+        }
+        Console.Clear();
         Console.Write("\n    To-Do List\n\n    Would you like to add a decription for the task? (Y/N) \n\n    ");
         string? taskdesc = Console.ReadLine();
         if (taskdesc == "Y" || taskdesc == "y") {
             Console.Clear();
             Console.Write("\n    To-Do List\n\n    What is the decription? \n\n    ");
             string? desc = Console.ReadLine();
-            ToDoDict.Add(task, desc);
+            ToDoList.Add(new TaskWithPriority(task, desc, taskLevel));
             Console.Clear();
-            Console.WriteLine("\n    To-Do List\n\n    Task was successfully added.");
+            Console.WriteLine("\n    To-Do List\n\n    Task was successfully added. \n\n    ");
             Console.ReadKey();
             mainMenu();
         } else {
-            ToDoDict.Add(task, "No Description");
+            ToDoList.Add(new TaskWithPriority(task, "No Description", taskLevel));
             Console.Clear();
-            Console.WriteLine("\n    To-Do List\n\n    Task was successfully added.");
+            Console.WriteLine("\n    To-Do List\n\n    Task was successfully added. \n\n    ");
             Console.ReadKey();
             mainMenu();
         }
@@ -75,13 +86,17 @@ void addTask(){
 void removeTask(){
     Console.Clear();
     Console.WriteLine("\n    To-Do List\n ");
-    if (ToDoDict.Count == 0) {
+    if (ToDoList.Count == 0) {
         Console.WriteLine("    This list is currently empty. ");
         Console.ReadKey();
         mainMenu();
     } else {
-        for (int i = 0; i < ToDoDict.Count; i++){
-            Console.WriteLine("    " + (i+1) + ". " + ToDoDict.ElementAt(i).Key + "\n       - " + ToDoDict.ElementAt(i).Value);
+        int i = 0;
+        ToDoList.Sort((x, y) => x.Priority.CompareTo(y.Priority));
+        foreach (var item in ToDoList)
+        {
+            i++;
+            Console.WriteLine($"    {i}. {item.TaskName} \n       - {item.TaskDesc}");
         }
     }
     Console.Write("\n    Which task would you like to remove? \n    If you want to go back, type 'Back'\n    ");
@@ -90,36 +105,39 @@ void removeTask(){
         mainMenu();
     } else {
         int taskRemove = Convert.ToInt32(backCheck);
-        if (taskRemove > ToDoDict.Count) {
+        if (taskRemove > ToDoList.Count) {
             Console.Clear();
-            Console.WriteLine("\n    To-Do List\n\n    That is not an option. Please Try Again. ");
+            Console.WriteLine("\n    To-Do List\n\n    That is not an option. Please Try Again.\n\n    ");
             Console.ReadKey();
             removeTask();
         } else {
-            ToDoDict.Remove(ToDoDict.ElementAt(taskRemove-1).Key);
+            ToDoList.Remove(ToDoList.ElementAt(taskRemove-1));
             Console.Clear();
-            Console.WriteLine("\n    To-Do List\n\n    Task was successfully removed.");
+            Console.WriteLine("\n    To-Do List\n\n    Task was successfully removed. \n\n    ");
             Console.ReadKey();
             Console.Clear();
             Console.WriteLine("\n    To-Do List\n ");
-            if (ToDoDict.Count == 0) {
+            if (ToDoList.Count == 0) {
                 Console.WriteLine("    This list is currently empty. ");
                 Console.ReadKey();
                 mainMenu();
             } else {
-                for (int i = 0; i < ToDoDict.Count; i++){
-                    Console.WriteLine("    " + (i+1) + ". " + ToDoDict.ElementAt(i).Key + "\n       - " + ToDoDict.ElementAt(i).Value);
+                int i = 0;
+                ToDoList.Sort((x, y) => x.Priority.CompareTo(y.Priority));
+                foreach (var item in ToDoList)
+                {
+                    i++;
+                    Console.WriteLine($"    {i}. {item.TaskName} \n       - {item.TaskDesc}");
                 }
             }
             Console.WriteLine("\n    If you would like to remove another task, Please type 'R'. ");
-            Console.WriteLine("\n    Press any other button to go back.");
+            Console.WriteLine("\n    Press any other button to go back.\n\n    ");
             string? again = Console.ReadLine();
             if(again == "R" || again == "r") {
                 removeTask();
             } else {
                 mainMenu();
-            }
-            
+            }           
         }
     }
 }
@@ -127,11 +145,15 @@ void removeTask(){
 void viewTask(){
     Console.Clear();
     Console.WriteLine("\n    To-Do List\n ");
-    if (ToDoDict.Count == 0) {
+    if (ToDoList.Count == 0) {
         Console.WriteLine("    This list is currently empty. \n    ");
     } else {
-        for (int i = 0; i < ToDoDict.Count; i++){
-            Console.WriteLine("    " + (i+1) + ". " + ToDoDict.ElementAt(i).Key + "\n       - " + ToDoDict.ElementAt(i).Value);
+        int i = 0;
+        ToDoList.Sort((x, y) => x.Priority.CompareTo(y.Priority));
+        foreach (var item in ToDoList)
+        {
+            i++;
+            Console.WriteLine($"    {i}. {item.TaskName} \n       - {item.TaskDesc}");
         }
     }
     Console.WriteLine("\n\n    Completed Tasks\n ");
@@ -140,10 +162,14 @@ void viewTask(){
         Console.ReadKey();
         mainMenu();
     } else {
-        for (int i = 0; i < ToDoComp.Count; i++){
-            Console.WriteLine("    " + (i+1) + ". " + ToDoComp.ElementAt(i).Key + "\n       - " + ToDoComp.ElementAt(i).Value);
+        int i = 0;
+        ToDoComp.Sort((x, y) => x.Priority.CompareTo(y.Priority));
+        foreach (var item in ToDoComp)
+        {
+            i++;
+            Console.WriteLine($"    {i}. {item.TaskName} \n       - {item.TaskDesc}");
         }
-        Console.WriteLine("\n    Press any button to go back.");
+        Console.WriteLine("\n    Press any button to go back.\n\n    ");
         Console.ReadKey();
         mainMenu();
     }
@@ -153,13 +179,17 @@ void viewTask(){
 void completeTask(){
     Console.Clear();
     Console.WriteLine("\n    To-Do List\n ");
-    if (ToDoDict.Count == 0) {
+    if (ToDoList.Count == 0) {
         Console.WriteLine("    This list is currently empty. ");
         Console.ReadKey();
         mainMenu();
     } else {
-        for (int i = 0; i < ToDoDict.Count; i++){
-            Console.WriteLine("    " + (i+1) + ". " + ToDoDict.ElementAt(i).Key + "\n       - " + ToDoDict.ElementAt(i).Value);
+        int i = 0;
+        ToDoList.Sort((x, y) => x.Priority.CompareTo(y.Priority));
+        foreach (var item in ToDoList)
+        {
+            i++;
+            Console.WriteLine($"    {i}. {item.TaskName} \n       - {item.TaskDesc}");
         }
     }
     Console.Write("\n    Which task would you like to set as completed? \n    If you want to go back, type 'Back'\n    ");
@@ -168,30 +198,34 @@ void completeTask(){
         mainMenu();
     } else {
         int taskRemove = Convert.ToInt32(backCheck);
-        if (taskRemove > ToDoDict.Count) {
+        if (taskRemove > ToDoList.Count) {
             Console.Clear();
-            Console.WriteLine("\n    To-Do List\n\n    That is not an option. Please Try Again. ");
+            Console.WriteLine("\n    To-Do List\n\n    That is not an option. Please Try Again.\n\n    ");
             Console.ReadKey();
-            removeTask();
+            completeTask();
         } else {
-            ToDoComp.Add(ToDoDict.ElementAt(taskRemove-1).Key, ToDoDict.ElementAt(taskRemove-1).Value);
-            ToDoDict.Remove(ToDoDict.ElementAt(taskRemove-1).Key);
+            ToDoComp.Add(ToDoList.ElementAt(taskRemove-1));
+            ToDoList.Remove(ToDoList.ElementAt(taskRemove-1));
             Console.Clear();
-            Console.WriteLine("\n    To-Do List\n\n    Task was successfully completed.");
+            Console.WriteLine("\n    To-Do List\n\n    Task was successfully completed.\n\n    ");
             Console.ReadKey();
             Console.Clear();
             Console.WriteLine("\n    To-Do List\n ");
-            if (ToDoDict.Count == 0) {
-                Console.WriteLine("    This list is currently empty. ");
+            if (ToDoList.Count == 0) {
+                Console.WriteLine("    This list is currently empty.\n\n    ");
                 Console.ReadKey();
                 mainMenu();
             } else {
-                for (int i = 0; i < ToDoDict.Count; i++){
-                    Console.WriteLine("    " + (i+1) + ". " + ToDoDict.ElementAt(i).Key + "\n       - " + ToDoDict.ElementAt(i).Value);
+                int i = 0;
+                ToDoList.Sort((x, y) => x.Priority.CompareTo(y.Priority));
+                foreach (var item in ToDoList)
+                {
+                    i++;
+                    Console.WriteLine($"    {i}. {item.TaskName} \n       - {item.TaskDesc}");
                 }
             }
             Console.WriteLine("\n    If you would like to complete another task, Please type 'R'. ");
-            Console.WriteLine("\n    Press any other button to go back.");
+            Console.WriteLine("\n    Press any other button to go back. \n\n    ");
             string? again = Console.ReadLine();
             if(again == "R" || again == "r") {
                 completeTask();
@@ -203,6 +237,19 @@ void completeTask(){
     }
 }
 
+public class TaskWithPriority
+{
+    public string TaskName { get; set; }
+    public string TaskDesc { get; set; }
+    public int Priority { get; set; }
+
+    public TaskWithPriority(string TaskName, string TaskDesc, int priority)
+    {
+        this.TaskName = TaskName;
+        this.TaskDesc = TaskDesc;
+        Priority = priority;
+    }
+}
 
 //Functions to add
 // Add Tasks [Done]
@@ -210,4 +257,4 @@ void completeTask(){
 // View Tasks [Done]
 // Add Description for Tasks [Done]
 // Add Completed Tasks [Done]
-// Add Priority
+// Add Priority [Done]
